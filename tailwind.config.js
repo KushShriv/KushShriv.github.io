7c1e44a954290a0a2103522668781fc3fa2fcc67
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2024-06-28 10:10:56 +0530

    Added Config Files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import CONFIG from './gitprofile.config';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    logs: false,
+    themes: [
+      ...CONFIG.themeConfig.themes,
+      { light: CONFIG.themeConfig.lightTheme },
+      { dark: CONFIG.themeConfig.darkTheme },
+    ],
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import CONFIG from './gitprofile.config';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: CONFIG.base || '/',
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          metaTitle: CONFIG.seo.title,
+          metaDescription: CONFIG.seo.description,
+          metaImageURL: CONFIG.seo.imageURL,
+        },
+      },
+    }),
+  ],
+  define: {
+    CONFIG: CONFIG,
+  },
+});
