4d390a8ef34139ec4271073f4e144d3dc61030d1
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2024-07-07 10:16:56 +0530

    Added Custom themes

---
+++ src/constants/default-dark-theme.tsx
@@ -0,0 +1,9 @@
+  export const DEFAULT_DARK_THEME = {
+    primary: '#DADADB',
+    secondary: '#414244',
+    accent: '#DADADB',
+    neutral: '#414244',
+    'base-100': '#1D1E20',
+    '--rounded-box': '0rem',
+    '--rounded-btn': '0rem',
+  };
---
+++ src/constants/default-light-theme.tsx
@@ -0,0 +1,9 @@
+export const DEFAULT_LIGHT_THEME = {
+  primary: '#1E1E1E',
+  secondary: '#D6D6D6',
+  accent: '#1E1E1E',
+  neutral: '#D6D6D6',
+  'base-100': '#F5F5F5',
+  '--rounded-box': '0rem',
+  '--rounded-btn': '0rem',
+};
