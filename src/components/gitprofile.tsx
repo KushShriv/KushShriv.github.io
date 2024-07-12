2578c2d3d0c1bc4910987a5161d15b8bf967488d
Kushaagra Shrivastava <134584075+KushShriv@users.noreply.github.com>
2024-07-12 10:10:50 +0530

    Added Main App

---
+++ src/components/gitprofile.tsx
@@ -0,0 +1,164 @@
+import { useCallback, useEffect, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { formatDistance } from 'date-fns';
+import {
+  CustomError,
+  GENERIC_ERROR,
+  INVALID_CONFIG_ERROR,
+  INVALID_GITHUB_USERNAME_ERROR,
+  setTooManyRequestError,
+} from '../constants/errors';
+
+/**
+ * Renders the GitProfile component.
+ *
+ * @param {Object} config - the configuration object
+ * @return {JSX.Element} the rendered GitProfile component
+ */
+const GitProfile = ({ config }: { config: Config }) => {
+  const [sanitizedConfig] = useState<SanitizedConfig | Record<string, never>>(
+    getSanitizedConfig(config),
+  );
+  const [theme, setTheme] = useState<string>(DEFAULT_THEMES[0]);
+  const [error, setError] = useState<CustomError | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [profile, setProfile] = useState<Profile | null>(null);
+  const [githubProjects, setGithubProjects] = useState<GithubProject[]>([]);
+
+  const getGithubProjects = useCallback(
+    async (publicRepoCount: number): Promise<GithubProject[]> => {
+      if (sanitizedConfig.projects.github.mode === 'automatic') {
+        if (publicRepoCount === 0) {
+          return [];
+        }
+
+        const excludeRepo =
+          sanitizedConfig.projects.github.automatic.exclude.projects
+            .map((project) => `+-repo:${project}`)
+            .join('');
+
+        const query = `user:${sanitizedConfig.github.username}+fork:${!sanitizedConfig.projects.github.automatic.exclude.forks}${excludeRepo}`;
+        const url = `https://api.github.com/search/repositories?q=${query}&sort=${sanitizedConfig.projects.github.automatic.sortBy}&per_page=${sanitizedConfig.projects.github.automatic.limit}&type=Repositories`;
+
+        const repoResponse = await axios.get(url, {
+          headers: { 'Content-Type': 'application/vnd.github.v3+json' },
+        });
+        const repoData = repoResponse.data;
+
+        return repoData.items;
+      } else {
+        if (sanitizedConfig.projects.github.manual.projects.length === 0) {
+          return [];
+        }
+        const repos = sanitizedConfig.projects.github.manual.projects
+          .map((project) => `+repo:${project}`)
+          .join('');
+
+        const url = `https://api.github.com/search/repositories?q=${repos}+fork:true&type=Repositories`;
+
+        const repoResponse = await axios.get(url, {
+          headers: { 'Content-Type': 'application/vnd.github.v3+json' },
+        });
+        const repoData = repoResponse.data;
+
+        return repoData.items;
+      }
+    },
+    [
+      sanitizedConfig.github.username,
+      sanitizedConfig.projects.github.mode,
+      sanitizedConfig.projects.github.manual.projects,
+      sanitizedConfig.projects.github.automatic.sortBy,
+      sanitizedConfig.projects.github.automatic.limit,
+      sanitizedConfig.projects.github.automatic.exclude.forks,
+      sanitizedConfig.projects.github.automatic.exclude.projects,
+    ],
+  );
+
+  const loadData = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get(
+        `https://api.github.com/users/${sanitizedConfig.github.username}`,
+      );
+      const data = response.data;
+
+      setProfile({
+        avatar: data.avatar_url,
+        name: data.name || ' ',
+        bio: data.bio || '',
+        location: data.location || '',
+        company: data.company || '',
+      });
+
+      if (!sanitizedConfig.projects.github.display) {
+        return;
+      }
+
+      setGithubProjects(await getGithubProjects(data.public_repos));
+    } catch (error) {
+      handleError(error as AxiosError | Error);
+    } finally {
+      setLoading(false);
+    }
+  }, [
+    sanitizedConfig.github.username,
+    sanitizedConfig.projects.github.display,
+    getGithubProjects,
+  ]);
+
+  useEffect(() => {
+    if (Object.keys(sanitizedConfig).length === 0) {
+      setError(INVALID_CONFIG_ERROR);
+    } else {
+      setError(null);
+      setTheme(getInitialTheme(sanitizedConfig.themeConfig));
+      loadData();
+    }
+  }, [sanitizedConfig, loadData]);
+
+  useEffect(() => {
+    theme && document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const handleError = (error: AxiosError | Error): void => {
+    console.error('Error:', error);
+
+    if (error instanceof AxiosError) {
+      try {
+        const reset = formatDistance(
+          new Date(error.response?.headers?.['x-ratelimit-reset'] * 1000),
+          new Date(),
+          { addSuffix: true },
+        );
+
+        if (typeof error.response?.status === 'number') {
+          switch (error.response.status) {
+            case 403:
+              setError(setTooManyRequestError(reset));
+              break;
+            case 404:
+              setError(INVALID_GITHUB_USERNAME_ERROR);
+              break;
+            default:
+              setError(GENERIC_ERROR);
+              break;
+          }
+        } else {
+          setError(GENERIC_ERROR);
+        }
+      } catch (innerError) {
+        setError(GENERIC_ERROR);
+      }
+    } else {
+      setError(GENERIC_ERROR);
+    }
+  };
+
+  return (
+    <></>
+  );
+};
+
+export default GitProfile;
